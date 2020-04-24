b52d201b06a7e8532844f9c45cecd0b07ffe3bd2
alextroitsky <alex2012.tr@gmail.com>
2020-04-24 18:09:30 +0300

    INIT

---
+++ src/server_side/MySerialServer.java
@@ -0,0 +1,11 @@
+package server_side;
+
+public class MySerialServer implements Server {
+
+    public void open(int port, ClientHandler c) {
+        // TODO: implement starting server to listen on port
+    }
+    public void close() {
+        // TODO: implement closing the listening server
+    }
+}
