14fbd1c14806d90102c45ee5386a8c1c9ed03b68
alextroitsky <alex2012.tr@gmail.com>
2020-04-29 22:59:36 +0300

    Added out/artifacts

---
+++ out/artifacts/PTM_projectTest_jar/TestSetter.java
@@ -0,0 +1,22 @@
+package test;
+
+import server_side.*;
+
+public class TestSetter {
+	
+	public static void setClasses(DesignTest dt){
+		
+		// set the server's Interface, e.g., "Server.class"
+		// don't forget to import the correct package e.g., "import server.Server"
+		dt.setServerInteface(Server.class);
+		// now fill in the other types according to their names
+		dt.setServerClass(MySerialServer.class);
+		dt.setClientHandlerInterface(ClientHandler.class);
+		dt.setClientHandlerClass(MyTestClientHandler.class);
+		dt.setCacheManagerInterface(CacheManager.class);
+		dt.setCacheManagerClass(FileCacheManager.class);
+		dt.setSolverInterface(Solver.class);
+	}
+	
+
+}
