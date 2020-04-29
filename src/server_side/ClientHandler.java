9bd81956b32940c16d4c564ece45e51da879d264
alextroitsky <alex2012.tr@gmail.com>
2020-04-29 21:52:43 +0300

    PTM1 STEPPING STONE 1 - finished

--- src/server_side/ClientHandler.java
+++ src/server_side/ClientHandler.java
@@ -4,5 +4,5 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public interface ClientHandler {
-    public void handleClient(InputStream in, OutputStream out);
+    public abstract void handleClient(InputStream in, OutputStream out);
 }
--- src/server_side/MyTestClientHandler.java
+++ src/server_side/MyTestClientHandler.java
@@ -3,9 +3,12 @@ package server_side;
 import java.io.InputStream;
 import java.io.OutputStream;
 
-public class MyTestClientHandler implements ClientHandler{
-        public void handleClient(InputStream in, OutputStream out) {
+public class MyTestClientHandler implements ClientHandler {
+    public Solver solver;
+    public CacheManager cm;
 
-        }
+    public void handleClient(InputStream in, OutputStream out) {
+
+    }
 
 }
